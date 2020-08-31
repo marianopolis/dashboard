cab7f6159b71489ad77570c8ec416ce8bd6d5454
stephen.zlu <stephen.lu.2002@gmail.com>
2020-08-30 22:15:34 -0400

    Added styling to cards and started chart

--- static/js/renderDashboard.js
+++ static/js/renderDashboard.js
@@ -1,25 +1,42 @@
-function createBootstrapRow(question, answer){
-	return `<div class="row">${createBootstrapCard(question, answer)}</div>`
+function createBootstrapCard(item){
+	return (
+		`<div class="card">
+			<div class="card-header">
+				<div class="badge badge-pill badge-info">${item.Status}</div>
+			</div>
+			<div class="card-body">
+				<h5 class="card-title">${item.Question}</h5>
+				<p class="card-text">${item.Answer}</p>
+				<p class="font-italic small float-right mt-2">Posted on ${item.Timestamp}</p>
+			</div>
+		</div>`
+	);
 }
 
-function createBootstrapCard(question, answer){
-	return `<div class="card" style="width: 18rem;">
-			  <div class="card-body">
-			    <h5 class="card-title">${question}</h5>
-			    <p class="card-text">${answer}</p>
-			  </div>
-			</div>`;
+function createCircle(item) {
+	return (
+		`
+			<img src="static/img/splash.png" class="circle" height="100" width="100">
+		`
+	)
 }
 
-
-function renderList(data){
+function renderChart(data) {
 	var htmlString = "";
 	for (const item of data){
 		if (item.Display==="TRUE"){
-				console.log(item.Question);
-				console.log(item.Answer);
-				htmlString += createBootstrapRow(item.Question, item.Answer);
-			}		
+				htmlString += createCircle(item);
+			}
+	}
+	return htmlString;
+}
+
+function renderList(data) {
+	var htmlString = "";
+	for (const item of data){
+		if (item.Display==="TRUE"){
+				htmlString += createBootstrapCard(item);
+			}
 	}
 	return htmlString;
 }
@@ -33,10 +50,8 @@ app.controller("dashboardController", function($scope, $http)
 
 			$http.get("api/get_questions").then(function(response) {
 
-				renderList(response.data.data);
-
-				$scope.message = renderList(response.data.data);
-				console.log(response);
+				$scope.chart = renderChart(response.data.data);
+				$scope.content = renderList(response.data.data);
 			})
 
 		}
